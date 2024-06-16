b5232a42507fac39347bb156994509d83dc5c114
yongwonZZ <shin082508@naver.com>
2024-06-17 05:04:22 +0900

    수정

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,22 +1,6 @@
-const express = require('express');
-const permission = require('../middlewares/permission');
-const userRouter = require('../routers/userRouter');
-const { signup, login, logout } = require('../services/userService');
-const orderRouter = require('../routers/orderRouter');
-const router = express.Router();
+// 이 routers 폴더에서 만들어진 모듈들을 깔끔하게 묶어주는 역할을 index.js가 함.
+// 나중에 import 할 때의 코드도 짧아지는 효과가 있음.
 
-// 로그인&아웃, 회원 가입
-router.post('/signup', signup); // 회원가입
-router.post('/login', login); // 로그인
-router.delete('/logout', logout); // 로그아웃
-
-// 각 라우터 연결
-router.use('/users', userRouter);
-router.use('/products', productsRouter);
-router.use('/orders', orderRouter);
-router.use('/categories', categorysRouter);
-
-//메인 페이지 및 상품 조회(비회원 기능)
-// router.get('/', categoryAndProducts); // 전체 카테고리 & 상품 목록 조회
-
-module.exports = router;
+// export * from './views-router';
+export * from './user-router';
+export * from './order-router';
