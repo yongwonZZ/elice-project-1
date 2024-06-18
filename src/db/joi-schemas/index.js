5440f6e3f1498bf6edf61cd0cdab97e5c8f81994
Haeun2 <chu1803@naver.com>
2024-06-18 17:57:16 +0900

    오류수정

---
+++ src/db/joi-schemas/index.js
@@ -0,0 +1,5 @@
+import { registerSchema, updateUserSchema } from './user-joi.js';
+import { orderSchema, updateOrderSchema } from './order-joi.js';
+import { productSchema, updateproductSchema } from './product-joi.js';
+
+export { registerSchema, updateUserSchema, orderSchema, updateOrderSchema };
