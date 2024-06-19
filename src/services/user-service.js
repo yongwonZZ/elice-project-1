af0220d8f78d00eab649ff092de4003711a829b3
[SW9] 정선경 <seonseon042@gmail.comgit>
2024-06-20 04:49:10 +0900

    사용자조회 추가

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,10 +1,8 @@
 import { userModel } from '../db/index.js';
-
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 class UserService {
-  // 본 파일의 맨 아래에서, new UserService(userModel) 하면, 이 함수의 인자로 전달됨
   constructor(userModel) {
     this.userModel = userModel;
   }
@@ -14,16 +12,13 @@ class UserService {
     if (!user) {
       throw new Error('유저가 없습니다.');
     }
-    const { email, fullName, phoneNumber, address } = user; // 수정: phoneNumber, address 추가
+    const { email, fullName, phoneNumber, address } = user;
     return { email, fullName, phoneNumber, address };
   }
 
-  // 회원가입
   async addUser(userInfo) {
-    // 객체 destructuring
     const { email, fullName, password } = userInfo;
 
-    // 이메일 중복 확인
     const user = await this.userModel.findByEmail(email);
     if (user) {
       throw new Error(
@@ -31,25 +26,15 @@ class UserService {
       );
     }
 
-    // 이메일 중복은 이제 아니므로, 회원가입을 진행함
-
-    // 우선 비밀번호 해쉬화(암호화)
     const hashedPassword = await bcrypt.hash(password, 10);
-
     const newUserInfo = { fullName, email, password: hashedPassword };
-
-    // db에 저장
     const createdNewUser = await this.userModel.create(newUserInfo);
 
     return createdNewUser;
   }
 
-  // 로그인
   async getUserToken(loginInfo) {
-    // 객체 destructuring
     const { email, password } = loginInfo;
-
-    // 우선 해당 이메일의 사용자 정보가  db에 존재하는지 확인
     const user = await this.userModel.findByEmail(email);
     if (!user) {
       throw new Error(
@@ -57,12 +42,7 @@ class UserService {
       );
     }
 
-    // 이제 이메일은 문제 없는 경우이므로, 비밀번호를 확인함
-
-    // 비밀번호 일치 여부 확인
-    const correctPasswordHash = user.password; // db에 저장되어 있는 암호화된 비밀번호
-
-    // 매개변수의 순서 중요 (1번째는 프론트가 보내온 비밀번호, 2번쨰는 db에 있떤 암호화된 비밀번호)
+    const correctPasswordHash = user.password;
     const isPasswordCorrect = await bcrypt.compare(
       password,
       correctPasswordHash
@@ -74,37 +54,32 @@ class UserService {
       );
     }
 
-    // 로그인 성공 -> JWT 웹 토큰 생성
     const secretKey = process.env.JWT_SECRET_KEY || 'secret-key';
-
-    // 2개 프로퍼티를 jwt 토큰에 담음
     const token = jwt.sign({ userId: user._id, role: user.role }, secretKey);
 
     return { token };
   }
 
-  // 사용자 목록을 받음.
-  async getUsers() {
-    const users = await this.userModel.findAll();
+  async getUsers({ page, perPage }) {
+    const users = await this.userModel.find({})
+      .skip((page - 1) * perPage)
+      .limit(perPage);
     return users;
   }
 
-  // 유저정보 수정, 현재 비밀번호가 있어야 수정 가능함.
-  async setUser(userInfoRequired, toUpdate) {
-    // 객체 destructuring
-    const { userId, currentPassword } = userInfoRequired;
+  async countUsers() {
+    const count = await this.userModel.countDocuments({});
+    return count;
+  }
 
-    // 우선 해당 id의 유저가 db에 있는지 확인
+  async setUser(userInfoRequired, toUpdate) {
+    const { userId, currentPassword } = userInfoRequired;
     let user = await this.userModel.findById(userId);
 
-    // db에서 찾지 못한 경우, 에러 메시지 반환
     if (!user) {
       throw new Error('가입 내역이 없습니다. 다시 한 번 확인해 주세요.');
     }
 
-    // 이제, 정보 수정을 위해 사용자가 입력한 비밀번호가 올바른 값인지 확인해야 함
-
-    // 비밀번호 일치 여부 확인
     const correctPasswordHash = user.password;
     const isPasswordCorrect = await bcrypt.compare(
       currentPassword,
@@ -117,37 +92,30 @@ class UserService {
       );
     }
 
-    // 이제 드디어 업데이트 시작
-
-    // 비밀번호도 변경하는 경우에는, 회원가입 때처럼 해쉬화 해주어야 함.
     const { password } = toUpdate;
-
     if (password) {
       const newPasswordHash = await bcrypt.hash(password, 10);
       toUpdate.password = newPasswordHash;
     }
 
-    // 업데이트 진행
     user = await this.userModel.update({
       userId,
       update: toUpdate,
     });
 
     const { email, fullName } = user;
-
     return { email, fullName };
   }
 
-  // 사용자 삭제
-async deleteUser(userId) {
-  const deletedUser = await this.userModel.deleteUser(userId);
-  if (!deletedUser) {
-    throw new Error('해당 ID의 사용자를 찾을 수 없습니다.');
-  }
-  return deletedUser;
+  async deleteUser(userId) {
+    const deletedUser = await this.userModel.deleteUser(userId);
+    if (!deletedUser) {
+      throw new Error('해당 ID의 사용자를 찾을 수 없습니다.');
+    }
+    return deletedUser;
   }
 }
 
 const userService = new UserService(userModel);
 
-export { userService }; 
+export { userService };
