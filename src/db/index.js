48a82c37888f2ff9da21b4b1fc37664426a55b81
[SW9] 정선경 <seonseon042@gmail.com>
2024-06-20 15:15:47 +0000

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 mongoose.set('strictQuery', true);
-mongoose.connect(MONGODB_URL);
+mongoose.connect(DB_URL);
 const db = mongoose.connection;
 
 db.on('connected', () =>
